d014fc170bb1629097e1014add16fc75cea5f079
PrabSG <43908117+PrabSG@users.noreply.github.com>
2019-02-20 15:41:42 +0000

    One-hot encoding successful, with unknown replacement done with mean average per column.

--- data_handling.py
+++ data_handling.py
@@ -1,71 +1,50 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
+import sklearn.preprocessing
 
 LABEL_ORDER_DICT = {
-    "education" :   ["primary", "secondary", "tertiary"],
-    "default"   :   ["no", "yes"],
-    "housing"   :   ["no", "yes"],
-    "loan"      :   ["no", "yes"],
-    "month"     :   ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],
+    "education": ["primary", "secondary", "tertiary"],
+    "default": ["no", "yes"],
+    "housing": ["no", "yes"],
+    "loan": ["no", "yes"],
+    "month": ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],
 }
 
 ORDER_LABELS = ["education", "default", "housing", "loan", "month"]
+CATEGORICAL_LABELS = ["job", "marital", "contact", "poutcome"]
 
-def labelEncodeOrderableFunc(label, categoryVal):
-    if not(categoryVal is None):
-        return LABEL_ORDER_DICT.get(label).index(categoryVal)
-
-labelEncodeOrderable = lambda label, x : np.nan if x is np.nan else 5
-
-
-# Read in the dataset
+# Read in the dataset.
 raw_data = pd.read_csv("bank-full.csv", delimiter=';')
 data_array = raw_data.get_values()
 
-# Separate the ground truth from the rest of the dataset
-y_series = raw_data.loc[:,'y']
+# Separate the ground truth from the rest of the dataset.
+y_series = raw_data.loc[:, 'y']
 X_dataframe = raw_data.iloc[:, :-1].drop(columns='duration')
-# X_dataframe.mask(X_dataframe == "unknown", np.nan)
-# print(X_dataframe.get_values()[1])
-#
-# for label in ORDER_LABELS:
-#     X_dataframe[label].apply(labelEncodeOrderable, args=(label,))
-#
+X_dataframe = X_dataframe.replace({"unknown": np.nan})
+ALL_LABELS = list(X_dataframe)
+print(ALL_LABELS)
 
-df = pd.DataFrame(np.arange(10).reshape(-1, 2), columns=['A', 'B'])
-print(df)
-df.replace(3, np.nan)
-print(df)
+print(X_dataframe.get_values()[1])
+# Replace those categorical variables that have an ordering using basic label encoding.
+for label in ORDER_LABELS:
+    X_dataframe[label] = X_dataframe[label].map(
+        lambda x: np.nan if x is np.nan else LABEL_ORDER_DICT.get(label).index(x), na_action='ignore')
 
+# One hot encoding of those categorical variables where a relative ordering is semantically incorrect.
+X_dataframe_oneHot = X_dataframe.copy()
+for label in CATEGORICAL_LABELS:
+    X_dataframe_oneHot = pd.get_dummies(X_dataframe_oneHot, columns=[label], prefix=[label])
 
+ONEHOT_LABELS = list(X_dataframe_oneHot)
 
+# Using the mean value to encode missing data.
+# Alternative method would be to build another model that could predict what the value may be,
+# depending on the other available attributes for that row of data.
+columnAvgs = {}
+for label in ONEHOT_LABELS:
+    columnAvgs[label] = X_dataframe_oneHot[label].mean(skipna=True)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print(list(raw_data))
+for label in ALL_LABELS:
+    sub_labels = filter(lambda sub: sub.startswith(label), X_dataframe_oneHot.columns)
+    for one_hot_subLabel in sub_labels:
+        X_dataframe_oneHot.loc[X_dataframe[label].isna(), one_hot_subLabel] = columnAvgs[one_hot_subLabel]
