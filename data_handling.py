1e6080aaef2ee50758d86b541a8276079760ee76
PrabSG <43908117+PrabSG@users.noreply.github.com>
2019-02-24 18:32:39 +0000

    Changed data handling to balance the data as number of yes's were significantly lower than no's

--- data_handling.py
+++ data_handling.py
@@ -2,30 +2,37 @@ import pandas as pd
 import numpy as np
 
 VAR_ORDER_DICT = {
-    "education": ["primary", "secondary", "tertiary"],
-    "default": ["no", "yes"],
-    "housing": ["no", "yes"],
-    "loan": ["no", "yes"],
+    "default": ["no", "unknown", "yes"],
+    "housing": ["no", "unknown", "yes"],
+    "loan": ["no", "unknown", "yes"],
     "month": ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],
 }
 
-ORDER_VARS = ["education", "default", "housing", "loan", "month"]
-CATEGORICAL_VARS = ["job", "marital", "contact", "poutcome"]
+ORDER_VARS = ["default", "housing", "loan", "month"]
+CATEGORICAL_VARS = ["job", "marital", "education", "contact", "poutcome"]
 
 # Read in the dataset, and shuffle it as it appears to be ordered by date.
 raw_data = pd.read_csv("bank-full.csv", delimiter=';')
-raw_data = raw_data.sample(frac=1).reset_index(drop=True)
+
+# Heavily imbalanced dataset where there are significantly more data where y="no".
+# So rebalancing the data now.
+num_of_yes = raw_data["y"].value_counts()[1]
+no_dataframe = raw_data[raw_data["y"] == "no"].sample(num_of_yes)
+yes_dataframe = raw_data[raw_data["y"] == "yes"]
+balanced_data = pd.concat([no_dataframe, yes_dataframe])
+
+# Shuffle rows of data
+balanced_data = balanced_data.sample(frac=1).reset_index(drop=True)
 
 # Separate the ground truth from the rest of the dataset.
-y_series = raw_data.loc[:, 'y']
-X_dataframe = raw_data.iloc[:, :-1].drop(columns='duration')
-X_dataframe = X_dataframe.replace({"unknown": np.nan})
+y_series = balanced_data.loc[:, 'y']
+X_dataframe = balanced_data.iloc[:, :-1].drop(columns='duration')
 ALL_VARS = list(X_dataframe)
 
 # Replace those categorical variables that have an ordering using basic label encoding.
 for col in ORDER_VARS:
     X_dataframe[col] = X_dataframe[col].map(
-        lambda x: np.nan if x is np.nan else VAR_ORDER_DICT.get(col).index(x), na_action='ignore')
+        lambda x: VAR_ORDER_DICT.get(col).index(x), na_action='ignore')
 
 # One-hot encoding of those categorical variables where a relative ordering is semantically incorrect.
 X_dataframe_oneHot = X_dataframe.copy()
@@ -34,24 +41,10 @@ for col in CATEGORICAL_VARS:
 
 ONEHOT_VARS = list(X_dataframe_oneHot)
 
-# Using the mean value to encode missing data.
-# Alternative method would be to build another model that could predict what the value may be,
-# depending on the other available attributes for that row of data.
-columnAvgs = {}
-for col in ONEHOT_VARS:
-    columnAvgs[col] = X_dataframe_oneHot[col].mean(skipna=True)
-
-# Locating the Nan values in the original dataframe, and then replacing it with the average for
-# the associated columns in the one-hot encoded data frame.
-for col in ALL_VARS:
-    sub_labels = filter(lambda sub: sub.startswith(col), X_dataframe_oneHot.columns)
-    for one_hot_subLabel in sub_labels:
-        X_dataframe_oneHot.loc[X_dataframe[col].isna(), one_hot_subLabel] = columnAvgs[one_hot_subLabel]
-
 # Encode the ground truth to be binary values
 y_series = y_series.map({"no" : 0, "yes" : 1})
 
 X = X_dataframe_oneHot.get_values()
 y = y_series.values
 
-np.savez("bank-full-shuffled-onehot", training_data=X, labels=y)
+np.savez("bank-full-balanced-shuffled-onehot-unknowns", training_data=X, labels=y)
--- nn-model.py
+++ nn-model.py
@@ -4,8 +4,23 @@ from tensorflow.keras.callbacks import TensorBoard
 import numpy as np
 import time
 
-data = np.load("bank-full-shuffled-onehot.npz")
+data = np.load("bank-full-balanced-shuffled-onehot-unknowns.npz")
 X = data['training_data']
 y = data['labels']
 
-#tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+NAME = "DNN-relu-128-256-128-softmax-{}".format(int(time.time()))
+tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+
+#Normalise the data
+#X = keras.utils.normalize(X, axis=-1, order=2)
+
+model = keras.Sequential()
+
+model.add(keras.layers.Dense(128, kernel_initializer="glorot_normal", bias_initializer="RandomNormal"))
+model.add(keras.layers.Activation("relu"))
+
+model.add(keras.layers.Dense(1))
+model.add(keras.layers.Activation("softmax"))
+
+model.compile(keras.optimizers.Adam(), loss="binary_crossentropy", metrics=["accuracy"])
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1, callbacks=[tensorboard])
