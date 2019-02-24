b48552203fdf500433aafa422900785e4471cb14
PrabSG <43908117+PrabSG@users.noreply.github.com>
2019-02-24 18:55:14 +0000

    Using the balanced data in a very basic 2-layer NN, not much success. Training dataset too small to learn off of

--- nn-model.py
+++ nn-model.py
@@ -8,19 +8,22 @@ data = np.load("bank-full-balanced-shuffled-onehot-unknowns.npz")
 X = data['training_data']
 y = data['labels']
 
-NAME = "DNN-relu-128-256-128-softmax-{}".format(int(time.time()))
+NAME = "DNN-relu-128-128-softmax-{}".format(int(time.time()))
 tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 
 #Normalise the data
-#X = keras.utils.normalize(X, axis=-1, order=2)
+X_normed = (X - X.min(0)) / X.ptp(0)
 
 model = keras.Sequential()
 
 model.add(keras.layers.Dense(128, kernel_initializer="glorot_normal", bias_initializer="RandomNormal"))
 model.add(keras.layers.Activation("relu"))
 
+model.add(keras.layers.Dense(128))
+model.add(keras.layers.Activation("relu"))
+
 model.add(keras.layers.Dense(1))
 model.add(keras.layers.Activation("softmax"))
 
 model.compile(keras.optimizers.Adam(), loss="binary_crossentropy", metrics=["accuracy"])
-model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1, callbacks=[tensorboard])
+model.fit(X_normed, y, batch_size=32, epochs=10, validation_split=0.1, callbacks=[tensorboard])
